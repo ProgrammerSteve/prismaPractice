dd8cddfac6b6b579dcd753a451ec33bbd65e74de
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-11-25 14:19:36 -0600

    altered the database schema and added relations

--- script.ts
+++ script.ts
@@ -1,16 +1,31 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-async function main(){
+async function main() {
   //you will write your prisma client queries here
-  const user=await prisma.user.create({data: {name:"Kyle"}})
+  await prisma.user.deleteMany()
+  const user = await prisma.user.create({
+    data: {
+      name: "Kyle",
+      email: "Kyle@test.com",
+      age: 27,
+      userPreference: {
+        create: {
+          emailUpdates: true
+        },
+      },
+    },
+    include: {
+      userPreference: true
+    }
+  })
   console.log(user)
 }
 
 main()
-  .catch(e=>{
+  .catch(e => {
     console.log(e.message)
   })
-  .finally(async()=>{
+  .finally(async () => {
     await prisma.$disconnect()
   })
